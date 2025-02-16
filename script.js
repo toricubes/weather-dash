1c89a4f1ab4682e09355520bbcf301a8120b50da
Tori <tori.cubells@gmail.com>
2025-02-16 18:17:10 -0500

    current weather

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 
 
 $(document).ready(function(){
-    let apiKey;
-    let currentWeatherEndpoint;
-    let fiveDayWeatherEndpoint;
+    let apiKey = 'a8399274d821a66647513527414df9b0';
+    let currentWeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+    let fiveDayWeatherEndpoint = "https://api.openweathermap.org/data/2.5/onecall?";
 
     let submitBtn = $('#submit');
     let searchField = $('#city-search');
+    let curTempDisp = $('.weather-data .current-temp');
+    let curHumDisp =  $('.weather-data .current-humidity');
+    let curUvDisp = $('.weather-data .current-uv');
+    let curIconDisp = $('#current-image');
+    let cityDisp = $('#current-weather-heading');
     
     function handleSubmit(event) {
         event.preventDefault();
@@ -16,7 +21,7 @@ $(document).ready(function(){
         if (!searchTerm) {
             return;
         }
-        let currentWeatherData = getCurrentWeather(searchTerm);
+        getCurrentWeather(searchTerm);
         // display current
         // display forecast
     }
@@ -25,7 +30,52 @@ $(document).ready(function(){
         let url = `${currentWeatherEndpoint}q=${searchTerm}&appid=${apiKey}`;
         // get weather data
         // return weather data as json object
+        let weatherJSON
+        fetch(url)
+        .then((res) => res.json())
+        .then((jsonRes) => {
+            displayCurrentWeather(jsonRes);
+        })
     }
+
+    function getFiveDay(lat, lon) {
+        let url = `${fiveDayWeatherEndpoint}lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${apiKey}`;
+
+        fetch(url)
+        .then((res) => res.json())
+        .then((jsonRes) => {
+            let uv = jsonRes.daily[0].uvi;
+            curUvDisp.html(uv);
+            displayFiveDay(jsonRes);
+        })
+        
+    }
+
+    function displayCurrentWeather(weatherJSON) {
+        let temp = weatherJSON.main.temp;
+        let icon = `https://openweathermap.org/img/wn/${weatherJSON.weather[0].icon}@4x.png`;
+        let city = weatherJSON.name;
+        let humidity = weatherJSON.main.humidity;
+        let lat = weatherJSON.coord.lat;
+        let lon = weatherJSON.coord.lon;
+
+
+        curTempDisp.html(temp);
+        curHumDisp.html(humidity);
+        curIconDisp.attr('src', icon);
+        cityDisp.html(`Current weather in ${city}`);
+
+        getFiveDay(lat,lon);
+
+    }
+
+    function displayFiveDay(weatherJSON) {
+        $('#forecast .wrapper').empty();
+        let card = $('<div>').addClass('card');
+        
+
+    }
+
 
     $(submitBtn).on('click', handleSubmit);
 
